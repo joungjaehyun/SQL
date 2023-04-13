37bfb45876c0411924b27dbf9b9d17c3ac7c50e6
joungjaehyun <joungjaehyun1008@gmail.com>
2023-04-13 20:46:13 +0900

    Update join_subquery.sql

--- exam/join_subquery.sql
+++ exam/join_subquery.sql
@@ -48,17 +48,45 @@ select (select cs.name
         from orders o
         group by o.custid;
 --(11) 고객의이름과고객이구매한도서목록
-select cs.name, b.bookname
-from orders o join book b,
-    on o.bookid = b.bookid
-              join customer cs
-    on o.custid = cs.custid;
+select cs.name,  b.bookname
+from customer cs join orders o
+            on cs.custid= o.custid
+            join book b
+            on o.bookid=b.bookid
+            order by cs.name asc;
 --(12) 도서의가격(Book 테이블)과판매가격(Orders 테이블)의차이가가장많은주문
+select *
+from book b join orders o
+on b.bookid = o.bookid
+where b.price - o.saleprice =
+(select max(b.price - o.saleprice)
+from orders o join book b
+on o.bookid=b.bookid);
+
 
 --(13) 도서의판매액평균보다자신의구매액평균이더높은고객의이름
 
 --3. 마당서점에서 다음의 심화된 질문에 대해 SQL 문을 작성하시오.
 
 --(1) 박지성이 구매한 도서의 출판사와 같은 출판사에서 도서를 구매한 고객의 이름
-
+select cs.name 
+from customer cs 
+            join orders o
+            on cs.custid = o.custid
+            join book b
+            on o.bookid = b.bookid
+            where cs.name  !='박지성' and b.publisher in (
+            select b.publisher
+            from book b join orders o
+            on b.bookid= o.bookid
+            where o.custid =1);
+            
 --(2) 두 개 이상의 서로 다른 출판사에서 도서를 구매한 고객의 이름
+select cs.name
+from customer cs join orders o
+                on cs.custid = o.custid
+                join book b
+                on o.bookid = b.bookid
+                group by cs.name
+                having count(distinct b.publisher)>=2
+                order by cs.name asc;
