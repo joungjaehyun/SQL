d1c6189412c2841540b1f66a7c18b2ff0d0ef72c
joungjaehyun <joungjaehyun1008@gmail.com>
2023-04-14 18:02:51 +0900

    Update subquery_exam.sql

--- exam/subquery_exam.sql
+++ exam/subquery_exam.sql
@@ -5,7 +5,8 @@ from emp where job = (select job from emp where empno=7788);
 
 -- 44. 사원번호가 7499인 사원보다 급여가 많은 사원을 표시하시오. 사원이름과 감당 업무
 select ename, job
-from emp where sal > (select sal from emp where empno=7499);
+from emp where sal > (select sal from emp where empno=7499)
+order by empno;
 
 -- 45. 최소급여를 받는 사원의 이름, 담당업무 및 급여를 표시하시오. (그룹함수 사용)
 select ename, job , sal
