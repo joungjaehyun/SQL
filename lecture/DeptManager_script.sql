957592d7a700cc74e99ce648d43eb72785a5511d
joungjaehyun <joungjaehyun1008@gmail.com>
2023-04-21 14:22:45 +0900

    Create DeptManager_script.sql

---
+++ lecture/DeptManager_script.sql
@@ -0,0 +1,24 @@
+
+
+
+-- DEPT MANAGER
+
+-- 1. List
+
+select * from dept;
+
+-- 2. search
+
+select * from dept where deptno=10;
+--select * from dept where deptno=?;
+select * from dept where dname ='ACCOUNTING';
+--select * from dept where dname =?;
+
+-- 3. insert
+insert into dept values (50, 'TEST', 'SEOUL');
+--insert into dept values (?, ?, ?)
+rollback;
+
+-- 4. 수정 update
+update dept set dname = 'AAA', loc = 'BBB' where deptno = 60;
+-- update dept set dname =?,loc =? where deptno =?;
